5d3bb86f17063e23f727b77dda661740dee07c4f
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2023-11-29 02:28:46 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <esp_system.h>
+#include <MultiPrinterLogger.hpp>
+#include <MultiPrinterLoggerInterface.hpp>
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Start");
+
+    Serial.printf("Minimum heap that has ever been available: %d\n", esp_get_minimum_free_heap_size());
+    Serial.printf("Before Initialize logger, free heap: %d\n", esp_get_free_heap_size());
+
+    MultiPrinterLogger *myLogger = new MultiPrinterLogger();
+    myLogger->addPrinter(&Serial);
+    myLogger->setLogLevel(MultiPrinterLoggerInterface::LogLevel::VERBOSE);
+    myLogger->setColorEnabled(true);
+
+    Log_Info(myLogger, "First Log_Info message.");
+    Log_Verbose(myLogger, "First Log_Verbose message.");
+    Log_Debug(myLogger, "First Log_Debug message.");
+    Log_Warning(myLogger, "First Log_Warning message.");
+    Log_Error(myLogger, "First Log_Error message.");
+
+    Serial.printf("Minimum heap that has ever been available: %d\n", esp_get_minimum_free_heap_size());
+    Serial.printf("After Initialize logger, free heap: %d\n", esp_get_free_heap_size());
+
+    delete myLogger;
+
+    Serial.printf("Minimum heap that has ever been available: %d\n", esp_get_minimum_free_heap_size());
+    Serial.printf("After Delete logger, free heap: %d\n", esp_get_free_heap_size());
+}
+
+void loop()
+{
+    delay(1000);
+}
